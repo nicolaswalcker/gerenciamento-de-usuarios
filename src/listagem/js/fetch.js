4efbfc8e63673856db6c0c99491a1adae48249ce
nicolaswalcker <walcker_@outlook.com>
2021-12-10 15:49:42 -0300

    feat: faz fetch dos dados

--- src/listagem/js/fetch.js
+++ src/listagem/js/fetch.js
@@ -1,13 +1,41 @@
 import axios from "axios";
-const fetch = () => {
-  const url = `https://reqres.in/api/users?page=2`;
+const container = document.querySelector(".js-container");
+const url = `https://reqres.in/api/users?page=1`;
+const fetch = (url) => {
   axios
     .get(url)
     .then((response) => {
+      const data = response.data.data;
+      data.forEach((user) => {
+        const {
+          email: userEmail,
+          first_name: firstName,
+          last_name: lastName,
+          avatar: image,
+        } = user;
+        const userFullName = `${firstName} ${lastName}`;
 
+        const personCard = `
+        <div class="c-card">
+        <a class="c-edit" href="#"
+          ><img src="../assets/icon-edit.svg" alt="Editar card"
+        /></a>
+        <div class="c-card__content">
+          <img
+            class="c-card__img"
+            src="${image}"
+            alt="Imagem de ${userFullName}"
+          />
+          <h2 class="c-card__title">${userFullName}</h2>
+          <p class="c-card__email">${userEmail}</p>
+        </div>
+      </div>
+        `;
+        container.innerHTML += personCard;
+      });
     })
     .catch((error) => {
       console.log(error);
     });
 };
-fetch()
+fetch(url);
